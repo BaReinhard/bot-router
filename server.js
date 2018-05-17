b87693a1db5725370655c35e7b3881f69a9108f6
BaReinhard <brett@reinhards.us>
2018-05-16 22:48:08 -0700

    Enabled Token Checking Middleware

--- server.js
+++ server.js
@@ -1,36 +1,38 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 var app = express();
 app.use(bodyParser.json());
 
-// app.use((req, res, next) => {
-//     console.log(req.body);
-//     if (req.body.token === process.env.GSUITE_TOKEN) {
-//         next();
-//     } else {
-//         res.status(401).send("Incorrect TOKEN provided, access denied");
-//     }
-// })
+app.use((req, res, next) => {
+  console.log(req.body);
+  if (req.body.token === process.env.GSUITE_TOKEN) {
+    next();
+  } else {
+    res.status(401).send("Incorrect TOKEN provided, access denied");
+  }
+});
 try {
-
-    // Handle post
-    app.post('/helpBot', (req, res) => {
-        axios.post('https://us-central1-uplifted-elixir-203119.cloudfunctions.net/helpBot', req.body).then(r => {
-            try {
-                console.log(r);
-                res.send(r)
-            } catch (e) {
-                console.log("Caught error", r.data)
-                res.send(r.data);
-            }
-        });
-
-    });
-
+  // Handle post
+  app.post("/helpBot", (req, res) => {
+    axios
+      .post(
+        "https://us-central1-uplifted-elixir-203119.cloudfunctions.net/helpBot",
+        req.body
+      )
+      .then(r => {
+        try {
+          console.log(r);
+          res.send(r);
+        } catch (e) {
+          console.log("Caught error", r.data);
+          res.send(r.data);
+        }
+      });
+  });
 } catch (err) {
-    console.log("Error Ocurred")
-    console.log(err);
+  console.log("Error Ocurred");
+  console.log(err);
 }
 
 app.listen(process.env.PORT || 8080);
